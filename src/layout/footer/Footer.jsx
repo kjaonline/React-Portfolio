a2ac75822674f01e4be96633916712d2fcd3b65a
Kris Alcordo <kalcordo@sufs.org>
2020-11-12 09:01:35 -0500

    Updated footer

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+library.add(fab)
 
-class Footer extends React.Component{
-	render(){
-		return(
-			<div className="footer">
-				<div>
-					Kris Alcordo. kris@alcordo.com
-				</div>
+
+function Footer(){
+	return(
+		<div className="footer">
+			<div class="disclaimer">
+				Kris Alcordo. kris@alcordo.com
 			</div>
-		)
-	}
+			<div className="links">
+				<ul>
+					<li>
+						<a href="https://instagram.com/krisalcordo"><FontAwesomeIcon icon={["fab", "instagram"]} /></a>
+					</li>
+					<li>
+						<a href="https://twitter.com/krisalcordo"><FontAwesomeIcon icon={["fab", "twitter"]} /></a>
+					</li>
+					<li>
+						<a href="https://facebook.com/krisalcordo"><FontAwesomeIcon icon={["fab", "facebook"]} /></a>
+					</li>
+					<li>
+						<a href="https://github.com/kjaonline"><FontAwesomeIcon icon={["fab", "github"]} /></a>
+					</li>
+					<li>
+						<a href="https://github.com/kjaonline"><FontAwesomeIcon icon={["fab", "linkedin"]} /></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	)
 }
 
 export default Footer;
