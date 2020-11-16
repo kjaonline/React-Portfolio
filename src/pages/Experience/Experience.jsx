adf8040f77838b4b4b72ef29ea9c3e35c5416756
Kris Alcordo <kalcordo@sufs.org>
2020-11-16 15:35:59 -0500

    Added resume

--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -4,6 +4,9 @@ function About() {
 	return(
 		<div className="pageWrapper">
 			<div className="page page-about">
+				<div className="full-resume">
+					<a href={process.env.PUBLIC_URL + '/public/ResumeKrisAlcordo2020.pdf'}>Click Here To Download My Full Resume</a>
+				</div>
 				<Experiences />
 			</div>
 		</div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Hero from '../../layout/hero/Hero'
 import Skills from './components/skills'
 
@@ -7,14 +8,14 @@ class Home extends React.Component{
 		return(
 			<div className="pageWrapper">
 				<div className="page page-home">
-					<Hero heroClass="home" text="Kris Alcordo" description="I am a Jacksonville based front web developer with an eye for design and a passion for functional interfaces and usability." />
-					<section className="section site-width">
-						<h2>Web Development</h2>
-						<p>After 8 years in a very fruitful Internet Marketing career, I decided to pursue my dream to be a programmer. After 2 years of teaching myself, I landed my first web development job in 2018. </p>
-						<Skills />
+				<Hero heroClass="home" text="Kris Alcordo" description="I am a Jacksonville based front web developer with an eye for design and a passion for functional interfaces and usability." />
+				<section className="section site-width">
+					<h2>Web Development</h2>
+					<p>After 8 years in a very fruitful Internet Marketing career, I decided to pursue my dream to be a programmer. After 2 years of teaching myself, I landed my first web development job in 2018. </p>
+					<Skills />
 
-					</section>
-				</div>
+				</section>
+			</div>
 			</div>
 		)
 	}
